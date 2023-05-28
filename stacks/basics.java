e98483fbaf2a1cc6ef72225aa408576e7578fbfe
Anushka <anushka30singh07@gmail.com>
2023-05-28 13:40:59 +0530

    basics

---
+++ stacks/basics.java
@@ -0,0 +1,16 @@
+package stacks;
+import java.util.Stack;
+import java.util.Collection;
+import java.io.*;
+
+
+public class basics {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack);
+    }
+   
+}

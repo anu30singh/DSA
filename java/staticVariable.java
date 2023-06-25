5f4b7fa337085ab5baedbe0a98356e79dfe5fa2b
Anushka <anushka30singh07@gmail.com>
2023-06-25 10:37:37 +0530

    java swings and practical

--- java/staticVariable.java
+++ java/staticVariable.java
@@ -1,17 +1,21 @@
 public class staticVariable {
-    static int a = m1();
-    static {
-        System.out.println("inside static block");
-    }
-    static int m1() {
-        System.out.println("from m1");
-        return 20 ;
-    }
+    
+    // static int a = m1();
+    // static {
+    //     System.out.println("inside static block");
+    // }
+    // static int m1() {
+    //     System.out.println("from m1");
+    //     return 20 ;
+    // }
     public static void main(String[] args) {
         System.out.println("value of a : " + a);
         System.out.println("from main");
+        System.out.println("Anushka Singh IT-C");
     }
 }
+
+
 
 
 
